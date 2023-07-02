8d509dd41612f757694a4e3b697a8d7ba0ba78f9
yosi <yosi.krismawardani@aqi.co.id>
2023-07-02 21:05:31 +0700

    installation 3

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    // setupNodeEvents(on, config) {
       // implement node event listeners here
+      baseUrl : 'https://kasirdemo.belajarqa.com',
+      specPattern : "cypress/support/e2e",
+      supportFile : false,
     },
   },
-});
+);

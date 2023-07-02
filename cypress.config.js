ff736da52f2c11134e12f1fb185c616794f4664b
yosi <yosi.krismawardani@aqi.co.id>
2023-07-02 22:20:55 +0700

    login with valid data

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
     // setupNodeEvents(on, config) {
       // implement node event listeners here
-      baseUrl : 'https://kasirdemo.belajarqa.com',
+      // baseUrl : 'https://kasirdemo.belajarqa.com',
       specPattern : "cypress/support/e2e",
       supportFile : false,
     },

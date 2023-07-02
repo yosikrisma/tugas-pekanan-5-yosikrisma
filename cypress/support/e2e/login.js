c6322aa802af42e18910b24397b2fe6eff9d7320
yosi <yosi.krismawardani@aqi.co.id>
2023-07-02 22:21:20 +0700

    login with valid data

---
+++ cypress/support/e2e/login.js
@@ -0,0 +1,15 @@
+describe('Login valid data', ()=>{
+    it('Open Apps', () => {
+        // Cypress.config('baseUrl');
+  
+        cy.visit('https://kasirdemo.belajarqa.com/');
+
+        cy.get('#email').type('yosikr03@gmail.com');
+
+        cy.get('#password').type('P@ssw0rd');
+
+        cy.get('.chakra-button').click();
+
+        cy.contains('kasirAja');
+    })
+})
